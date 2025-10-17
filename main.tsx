a6327c06370adf8c39e8e2d1c1ce666cd964c2f5
NandanaBabu125 <nandanababuc@gmail.com>
2025-10-17 14:32:12 +0530

    ADEED SRC SEPARATE

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Use HashRouter for GitHub Pages
+import App from "./App.tsx";
+import "./index.css";
+import "./hideBadge.css";
+
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
